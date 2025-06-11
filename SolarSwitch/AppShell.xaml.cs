5d972e5fbb528bdf20f25d66253cbc7999b83f8d
Bittner Markus <Markus.Bittner@hugo-mueller.de>
2025-06-11 11:54:44 +0200

    Initial commit

---
+++ SolarSwitch/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarSwitch;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SolarSwitch/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SolarSwitch;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
